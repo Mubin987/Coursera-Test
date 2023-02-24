86b0dc4c55ec4a97c78d40dd73bc68821375f72a
Mubin987 <99292441+Mubin987@users.noreply.github.com>
2023-02-24 16:39:23 +0500

    events added

--- Practice/javascript/DOM/DOM.js
+++ Practice/javascript/DOM/DOM.js
@@ -1,6 +1,11 @@
 //console.log(document instanceof HTMLDocument)
 //console.log(document.getElementById("title"))
-function welcome(){
+
+document.addEventListener("DOMContentLoaded",
+function(event){
+function welcome(event){
+    console.log(event);
+    this.textContent="Confirmed";
     console.log("Welcome "+document.getElementById("name").value);
     var name1 = document.querySelector("#name").value;
     var msg = "<h2>Welcome " + name1 + "<h2>";
@@ -11,3 +16,16 @@ function welcome(){
         document.querySelector("#title").textContent=name1;
     }
 }
+
+//  document.querySelector("button").addEventListener("click",welcome);
+ document.querySelector("button").onclick=welcome;
+ document.querySelector("body").addEventListener("mousemove",
+ function(event){
+    if(event.shiftKey===true){
+        console.log("x: ",event.clientX);
+        console.log("y: ",event.clientY);
+    }
+ }
+ );
+}
+);
