9b2d47c1349a7ccb03541c870991315b010854b3
Mubin987 <99292441+Mubin987@users.noreply.github.com>
2023-06-02 15:35:05 +0500

    commit

--- mini-projects/Quiz/main.js
+++ mini-projects/Quiz/main.js
@@ -9,7 +9,7 @@ function quiz(){
     heading.textContent = "Javascript Quiz"
     document.getElementById("bodyy").appendChild(heading)
     var Questions = ["Q.1) Which of the following is not a valid JavaScript data type?","Q.2) What is the correct way to declare a JavaScript variable?","Q.3) What is the result of the following expression: \"2\" + 2?","Q.4) How do you write a comment in JavaScript?","Q.5) Which method is used to remove the last element from an array in JavaScript?"]
-    var choices = [['String','Boolean','Float','Function'],['var = myVariable;','variable myVariable;','myVariable;','declare myVariable;'],['4','\"22\"','\"4\"','TypeError'],['<!-- This is a comment -->','/* This is a comment */','// This is a comment','** This is a comment **'],['pop()','remove()','delete()','shift()']]
+    var choices = [['String','Boolean','Float','Function'],['var 7 = myVariable;','variable myVariable = 7;','myVariable = 7;','var myVariable = 7;'],['4','\"22\"','\"4\"','TypeError'],['<!-- This is a comment -->','/* This is a comment */','// This is a comment','** This is a comment **'],['pop()','remove()','delete()','shift()']]
     for(var i=0;i<5;i++){
         var question = document.createElement("div")
         question.textContent = Questions[i]
@@ -50,7 +50,7 @@ function quiz(){
 // }
 
 function results(){
-    var answers = ['Float','myVariable;','"22"','// This is a comment','pop()']
+    var answers = ['Float','var myVariable = 7;','"22"','// This is a comment','pop()']
     // console.log("r")
    // do{
         submitPressed = false;
@@ -76,7 +76,8 @@ function results(){
         alert("Answer all questions");
     }
     else{
-        alert('Your Score is ' + score)
+        alert('Congratulations🎉🎉\nYou got ' + score + ' correct')
+        location.reload();
     }
     
 }
@@ -101,8 +102,8 @@ function startTimer() {
     displayTime(minutes, seconds);
 
     if (secondsRemaining <= 0) {
-      clearInterval(countdown); 
-      alert("Time's up!"); 
+      clearInterval(countdown);
+      alert("Time's up!");
     }
   }, 1000); 
 }
